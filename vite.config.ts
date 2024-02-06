78ec5abdd185d9979a1258aecd497b2671d81028
wjk_1998 <wangjiankun_moreji@163.com>
2024-02-06 10:16:55 +0800

    fix: 页面打包

--- vite.config.ts
+++ vite.config.ts
@@ -5,23 +5,4 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
-  build: {
-    lib: {
-      entry: "package/index.ts",
-      fileName(format) {
-        return `index.${format}.js`;
-      },
-      name: "quick-tour",
-      formats: ["cjs", "es", "umd"],
-    },
-    rollupOptions: {
-      external: ["vue"],
-      output: {
-        globals: {
-          vue: "Vue",
-        },
-      },
-    },
-    outDir: "dist",
-  },
 });
